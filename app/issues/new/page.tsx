e97b12aa35e2493d3bfadc2824300f553c7b0b86
israel <akinleyeisrael12@gmail.com>
2023-10-17 14:38:58 -0400

    Handle form submission

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,17 +1,39 @@
 'use client'
- 
-import { Button, TextArea, TextField } from '@radix-ui/themes'
-import React from 'react'
+
+import { Button, TextField } from '@radix-ui/themes'
+import SimpleMDE from 'react-simplemde-editor'
+import "easymde/dist/easymde.min.css"
+import { useForm , Controller} from 'react-hook-form'
+import axios from 'axios'
+import { useRouter } from 'next/navigation'
+
+interface IssueForm{
+  title: string;
+  description: string;
+}
+
 
 const NewIssuePage = () => {
+  const router = useRouter();
+  const {register, control, handleSubmit} = useForm<IssueForm>();
   return (
-    <div className='max-w-xl space-y-3'>
-        <TextField.Root>
-            <TextField.Input placeholder='Title'/>
-        </TextField.Root>
-        <TextArea placeholder='Description'></TextArea>
-        <Button>Submit New Issue</Button>
-    </div>
+    <form 
+    className='max-w-xl space-y-3' 
+    onSubmit={handleSubmit((data)=> {
+      axios.post('/api/issues', data);
+      router.push('/issues');
+    })}>
+      <TextField.Root>
+        <TextField.Input placeholder='Title' {...register('title')}/>
+      </TextField.Root>
+      <Controller
+      name="description"
+      control={control}
+      render={({field}) => <SimpleMDE placeholder='Description'{...field}/>}
+      />
+      
+      <Button>Submit New Issue</Button>
+    </form>
   )
 }
 
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,15 @@
 import '@radix-ui/themes/styles.css'
 import './globals.css'
+import './theme-config.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-import { Theme } from '@radix-ui/themes'
+import { Theme, ThemePanel } from '@radix-ui/themes'
 import NavBar from './NavBar'
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ 
+  subsets: ['latin'],
+  variable: '--font-inter',
+ })
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -19,13 +23,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-        <Theme>
-        <NavBar />
-        <main className='p-5'>
-          {children}
-        </main>
-
+      <body className={inter.variable}>
+        <Theme accentColor="violet">
+          <NavBar />
+          <main className='p-5'>
+            {children}
+          </main>
         </Theme>
       </body>
     </html>
