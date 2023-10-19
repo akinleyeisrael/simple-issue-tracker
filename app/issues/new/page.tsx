da79cc536978141c28d03772f07001b51bdeb1ac
israel <akinleyeisrael12@gmail.com>
2023-10-18 20:46:50 -0400

    added test remote

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,69 +1,71 @@
-'use client'
+"use client";
 
-import { Button, Callout, Text, TextField } from '@radix-ui/themes'
-import SimpleMDE from 'react-simplemde-editor'
-import "easymde/dist/easymde.min.css"
-import { useForm, Controller } from 'react-hook-form'
-import axios from 'axios'
-import { useRouter } from 'next/navigation'
-import { useState } from 'react'
-import { zodResolver } from '@hookform/resolvers/zod'
-import { createIssueSchema } from '@/app/valiadationSchemas'
-import z from 'zod'
-import ErrorMessage from '@/app/components/ErrorMessage'
-import Spinner from '@/app/components/Spinner'
+import { Button, Callout, Text, TextField } from "@radix-ui/themes";
+import SimpleMDE from "react-simplemde-editor";
+import "easymde/dist/easymde.min.css";
+import { useForm, Controller } from "react-hook-form";
+import axios from "axios";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { createIssueSchema } from "@/app/valiadationSchemas";
+import z from "zod";
+import ErrorMessage from "@/app/components/ErrorMessage";
+import Spinner from "@/app/components/Spinner";
 
 // Generate interface based on schema
 type IssueForm = z.infer<typeof createIssueSchema>;
 
-
 const NewIssuePage = () => {
   const router = useRouter();
-  const { register, control, handleSubmit, formState: { errors } } = useForm<IssueForm>({
-    resolver: zodResolver(createIssueSchema)
+  const {
+    register,
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IssueForm>({
+    resolver: zodResolver(createIssueSchema),
   });
-  const [error, setError] = useState('');
+  const [error, setError] = useState("");
   const [isSubmitting, setSubmitting] = useState(false);
 
-  const onSubmit = handleSubmit(async(data) => {
+  const onSubmit = handleSubmit(async (data) => {
     try {
       setSubmitting(true);
-      await axios.post('/api/issues', data);
-      router.push('/issues');
+      await axios.post("/api/issues", data);
+      router.push("/issues");
     } catch (error) {
       setSubmitting(false);
-      setError('An unexpected error occured.');
+      setError("An unexpected error occuredd.");
     }
-  })
+  });
 
   return (
-    <div className='max-w-xl space-y-3'>
-      {error && <Callout.Root color='red' className='mb-5'>
-        <Callout.Text>{error}</Callout.Text>
-      </Callout.Root>}
-      <form
-        className='max-w-xl space-y-3'
-        onSubmit={onSubmit}
-      >
+    <div className="max-w-xl space-y-3">
+      {error && (
+        <Callout.Root color="red" className="mb-5">
+          <Callout.Text>{error}</Callout.Text>
+        </Callout.Root>
+      )}
+      <form className="max-w-xl space-y-3" onSubmit={onSubmit}>
         <TextField.Root>
-          <TextField.Input placeholder='Title' {...register('title')} />
+          <TextField.Input placeholder="Title" {...register("title")} />
         </TextField.Root>
-        <ErrorMessage>
-          {errors.title?.message}
-        </ErrorMessage>
+        <ErrorMessage>{errors.title?.message}</ErrorMessage>
         <Controller
           name="description"
           control={control}
-          render={({ field }) =>
-            <SimpleMDE placeholder='Description'{...field} />}
+          render={({ field }) => (
+            <SimpleMDE placeholder="Description" {...field} />
+          )}
         />
-        <ErrorMessage>
-          {errors.description?.message}
-        </ErrorMessage>
-        <Button disabled={isSubmitting}>Submit New Issue {isSubmitting && <Spinner />}</Button>
+        <ErrorMessage>{errors.description?.message}</ErrorMessage>
+        <Button disabled={isSubmitting}>
+          Submit New Issue {isSubmitting && <Spinner />}
+        </Button>
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default NewIssuePage
+export default NewIssuePage;
--- prisma/client.ts
+++ prisma/client.ts
@@ -1,4 +1,5 @@
 import { PrismaClient } from '@prisma/client'
+// import { PrismaClient } from '../prisma/generated/clientPg'
 
 const prismaClientSingleton = () => {
   return new PrismaClient()
